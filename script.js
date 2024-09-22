25a0635a6cc5e69d7cb688272a25bc8aab350a2f
jimmysungod <44864856+jimmysungod@users.noreply.github.com>
2024-09-22 22:53:51 +0800

    TEST

--- script.js
+++ script.js
@@ -19,17 +19,35 @@ function convertJianpu() {
 
     const formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
     voice.draw(context, stave);
-
-    // 生成 MIDI 並播放
-    generateMIDI(jianpuInput, keySignature);
 }
 
 function jianpuToVexFlow(jianpu, keySignature) {
     const VF = Vex.Flow;
     const notes = jianpu.split(' ').map((note, index) => {
+        let duration = 'q';
+        let keys = [];
+
+        if (note === '0') {
+            // 休止符
+            keys = ['b/4'];
+            duration = 'qr';
+        } else {
+            // 處理高音和低音
+            let octave = 4;
+            if (note.includes('.')) {
+                octave = 5;
+                note = note.replace('.', '');
+            } else if (note.includes(',')) {
+                octave = 3;
+                note = note.replace(',', '');
+            }
+            keys = [noteToKey(note, keySignature, octave)];
+        }
+
+	
         const staveNote = new VF.StaveNote({
-            keys: [noteToKey(note, keySignature)],
-            duration: 'q'
+            keys: keys,
+            duration: duration
         });
 
         // 添加簡譜標記
@@ -47,69 +65,40 @@ function jianpuToVexFlow(jianpu, keySignature) {
     return notes;
 }
 
-function noteToKey(note, keySignature) {
+function noteToKey(note, keySignature, octave = 4) {
     const keyMappings = {
-        'C': { '1': 'c/4', '2': 'd/4', '3': 'e/4', '4': 'f/4', '5': 'g/4', '6': 'a/4', '7': 'b/4' },
-        'G': { '1': 'g/4', '2': 'a/4', '3': 'b/4', '4': 'c/5', '5': 'd/5', '6': 'e/5', '7': 'f#/5' },
-        'D': { '1': 'd/4', '2': 'e/4', '3': 'f#/4', '4': 'g/4', '5': 'a/4', '6': 'b/4', '7': 'c#/5' },
-        'A': { '1': 'a/4', '2': 'b/4', '3': 'c#/5', '4': 'd/5', '5': 'e/5', '6': 'f#/5', '7': 'g#/5' },
-        'E': { '1': 'e/4', '2': 'f#/4', '3': 'g#/4', '4': 'a/4', '5': 'b/4', '6': 'c#/5', '7': 'd#/5' },
-        'B': { '1': 'b/4', '2': 'c#/5', '3': 'd#/5', '4': 'e/5', '5': 'f#/5', '6': 'g#/5', '7': 'a#/5' },
-        'F#': { '1': 'f#/4', '2': 'g#/4', '3': 'a#/4', '4': 'b/4', '5': 'c#/5', '6': 'd#/5', '7': 'e#/5' },
-        'C#': { '1': 'c#/4', '2': 'd#/4', '3': 'e#/4', '4': 'f#/4', '5': 'g#/4', '6': 'a#/4', '7': 'b#/4' },
-        'F': { '1': 'f/4', '2': 'g/4', '3': 'a/4', '4': 'bb/4', '5': 'c/5', '6': 'd/5', '7': 'e/5' },
-        'Bb': { '1': 'bb/4', '2': 'c/5', '3': 'd/5', '4': 'eb/5', '5': 'f/5', '6': 'g/5', '7': 'a/5' },
-        'Eb': { '1': 'eb/4', '2': 'f/4', '3': 'g/4', '4': 'ab/4', '5': 'bb/4', '6': 'c/5', '7': 'd/5' },
-        'Ab': { '1': 'ab/4', '2': 'bb/4', '3': 'c/5', '4': 'db/5', '5': 'eb/5', '6': 'f/5', '7': 'g/5' },
-        'Db': { '1': 'db/4', '2': 'eb/4', '3': 'f/4', '4': 'gb/4', '5': 'ab/4', '6': 'bb/4', '7': 'c/5' },
-        'Gb': { '1': 'gb/4', '2': 'ab/4', '3': 'bb/4', '4': 'cb/5', '5': 'db/5', '6': 'eb/5', '7': 'f/5' },
-        'Cb': { '1': 'cb/4', '2': 'db/4', '3': 'eb/4', '4': 'fb/4', '5': 'gb/4', '6': 'ab/4', '7': 'bb/4' }
+        'C': { '1': 'c', '2': 'd', '3': 'e', '4': 'f', '5': 'g', '6': 'a', '7': 'b' },
+        'G': { '1': 'g', '2': 'a', '3': 'b', '4': 'c', '5': 'd', '6': 'e', '7': 'f#' },
+        'D': { '1': 'd', '2': 'e', '3': 'f#', '4': 'g', '5': 'a', '6': 'b', '7': 'c#' },
+        'A': { '1': 'a', '2': 'b', '3': 'c#', '4': 'd', '5': 'e', '6': 'f#', '7': 'g#' },
+        'E': { '1': 'e', '2': 'f#', '3': 'g#', '4': 'a', '5': 'b', '6': 'c#', '7': 'd#' },
+        'B': { '1': 'b', '2': 'c#', '3': 'd#', '4': 'e', '5': 'f#', '6': 'g#', '7': 'a#' },
+        'F#': { '1': 'f#', '2': 'g#', '3': 'a#', '4': 'b', '5': 'c#', '6': 'd#', '7': 'e#' },
+        'C#': { '1': 'c#', '2': 'd#', '3': 'e#', '4': 'f#', '5': 'g#', '6': 'a#', '7': 'b#' },
+        'F': { '1': 'f', '2': 'g', '3': 'a', '4': 'bb', '5': 'c', '6': 'd', '7': 'e' },
+        'Bb': { '1': 'bb', '2': 'c', '3': 'd', '4': 'eb', '5': 'f', '6': 'g', '7': 'a' },
+        'Eb': { '1': 'eb', '2': 'f', '3': 'g', '4': 'ab', '5': 'bb', '6': 'c', '7': 'd' },
+        'Ab': { '1': 'ab', '2': 'bb', '3': 'c', '4': 'db', '5': 'eb', '6': 'f', '7': 'g' },
+        'Db': { '1': 'db', '2': 'eb', '3': 'f', '4': 'gb', '5': 'ab', '6': 'bb', '7': 'c' },
+        'Gb': { '1': 'gb', '2': 'ab', '3': 'bb', '4': 'cb', '5': 'db', '6': 'eb', '7': 'f' },
+        'Cb': { '1': 'cb', '2': 'db', '3': 'eb', '4': 'fb', '5': 'gb', '6': 'ab', '7': 'bb' }
     };
 
     const mapping = keyMappings[keySignature] || keyMappings['C'];
-    return mapping[note.replace('#', '').replace('b', '')] || 'c/4';
+    return mapping[note.replace('#', '').replace('b', '')] + '/' + octave;
 }
 
-function generateMIDI(jianpu, keySignature) {
-    const midi = new Midi();
-    const track = new Track();
-    midi.addTrack(track);
+function playMIDI() {
+    const jianpuInput = document.getElementById('jianpuInput').value;
+    const keySignature = document.getElementById('keySignature').value;
+    const notes = jianpuToVexFlow(jianpuInput, keySignature);
 
-    const keyMappings = {
-        'C': { '1': 60, '2': 62, '3': 64, '4': 65, '5': 67, '6': 69, '7': 71 },
-        'G': { '1': 67, '2': 69, '3': 71, '4': 72, '5': 74, '6': 76, '7': 78 },
-        'D': { '1': 62, '2': 64, '3': 66, '4': 67, '5': 69, '6': 71, '7': 73 },
-        'A': { '1': 69, '2': 71, '3': 73, '4': 74, '5': 76, '6': 78, '7': 80 },
-        'E': { '1': 64, '2': 66, '3': 68, '4': 69, '5': 71, '6': 73, '7': 75 },
-        'B': { '1': 71, '2': 73, '3': 75, '4': 76, '5': 78, '6': 80, '7': 82 },
-        'F#': { '1': 66, '2': 68, '3': 70, '4': 71, '5': 73, '6': 75, '7': 77 },
-        'C#': { '1': 61, '2': 63, '3': 65, '4': 66, '5': 68, '6': 70, '7': 72 },
-        'F': { '1': 65, '2': 67, '3': 69, '4': 70, '5': 72, '6': 74, '7': 76 },
-        'Bb': { '1': 70, '2': 72, '3': 74, '4': 75, '5': 77, '6': 79, '7': 81 },
-        'Eb': { '1': 63, '2': 65, '3': 67, '4': 68, '5': 70, '6': 72, '7': 74 },
-        'Ab': { '1': 68, '2': 70, '3': 72, '4': 73, '5': 75, '6': 77, '7': 79 },
-        'Db': { '1': 61, '2': 63, '3': 65, '4': 66, '5': 68, '6': 70, '7': 72 },
-        'Gb': { '1': 66, '2': 68, '3': 70, '4': 71, '5': 73, '6': 75, '7': 77 },
-        'Cb': { '1': 59, '2': 61, '3': 63, '4': 64, '5': 66, '6': 68, '7': 70 }
-    };
-
-    const mapping = keyMappings[keySignature] || keyMappings['C'];
-    const notes = jianpu.split(' ');
-
-    notes.forEach(note => {
-        const midiNote = mapping[note.replace('#', '').replace('b', '')];
-        if (midiNote) {
-            track.addEvent(new NoteEvent({ pitch: [midiNote], duration: 64 }));
+    const synth = new Tone.Synth().toDestination();
+    const now = Tone.now();
+    notes.forEach((note, index) => {
+        if (note.keys[0] !== 'b/4') { // 跳過休止符
+            const midiNote = note.keys[0].replace('/', '');
+            synth.triggerAttackRelease(midiNote, '8n', now + index * 0.5);
         }
     });
-
-    const midiData = midi.toBytes();
-    const blob = new Blob([new Uint8Array(midiData)], { type: 'audio/midi' });
-    const url = URL.createObjectURL(blob);
-
-    const downloadLink = document.getElementById('downloadLink');
-    downloadLink.href = url;
-
-    // 使用 MIDI.js 播放生成的 MIDI 文件
-    MIDIjs.play(url);
 }
